fe7435c6a33a786531e7acaeae1b82bd18dd2e5f
RAHUL KUMAR <87722928+rahulkumarmmmut@users.noreply.github.com>
2024-02-06 21:23:22 -0500

    Create 1768_Merge_Strings_Alternately.py

---
+++ Array_Strings/1768_Merge_Strings_Alternately.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        s = ""
+        shorts = min( len(word1), len(word2))
+
+        for i in range(shorts):
+            s = s + word1[i] + word2[i]
+        if len(word1) > len(word2):
+            s = s + word1[shorts:]
+        else:
+            s = s + word2[shorts:]
+        return s
